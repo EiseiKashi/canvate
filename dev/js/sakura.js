acedcea0a610068b5a5a0e0859ebaed1e9fc7b6e
EiseiKashi <eisei.kashi@sakuracode.com>
2018-03-28 19:43:09 -0300

    Update test

--- dev/js/sakura.js
+++ dev/js/sakura.js
@@ -1,61 +1,74 @@
-isDebug = true;
-var canvas = document.createElement("canvas");
-document.body.appendChild(canvas);
-
-canvas.width  = 800;
-canvas.height = 300;
-
-canvas.style.border = "solid 0.5px gray";
-var canvatte = new Canvate(canvas);
-var hanaList = [];
 var HanaSakura = function(image){
-    debugger
-    var clip = canvatte.addNewClip(image);
-    clip.x = 10//(Math.random()*(canvas.width/2)) +canvas.width/2;
-    clip.y = 10//(Math.random()*(canvas.height/2))+canvas.height/2;
+    var maxX   = mainCanvas.width
+    var maxY   = mainCanvas.height/2
+    var clip   = canvatte.addNewClip(image);
+
+        clip.width  = 15;
+        clip.height = 15;
+        clip.x      = maxX;
+        clip.y      = (Math.random()*maxY);
     
-    var speedX = Math.random()*30;
-    var speedY = Math.random()*30;
+    var speedX = (Math.random()*30)+15;
+    var speedY = Math.random()*10;
+
     this.move = function(){
-        clip.x += speedX;
+        clip.x -= speedX;
         clip.y += speedY;
-        clip.rotation += 0.5;
+        clip.rotation += 1;
+        if(clip.x < 0 || clip.x > 800){
+            speedX *= -1;
+        }
+        if(clip.y < 0 || clip.y > 300){
+            speedY *= -1;
+        }
     }
 }
-var added = {};
 
 function debug (inner, name){
     if(added[name] == undefined){
-        var p = document.createElement("p");
+        var p           = document.createElement("p");
             p.innerHTML = name
         var section = document.createElement("section");
             section.appendChild(p);
             section.appendChild(inner)
         document.body.appendChild(section);
-        added[name]  = true;
+        added[name] = true;
     }
 }
 
 function onImageLoaded(event){
-    debugger
     imageClip.crop(0,900,250,250);
-    var totalSakura = 1;
+    var totalSakura = 1500;
     for(var index = 0; index < totalSakura; index++){
         var hana = new HanaSakura(imageClip.canvas);
         hanaList.push(hana);
     }
     
-   // setInterval(sakuraFubuki, 50);
+    window.requestAnimationFrame(sakuraFubuki);
 }
-
-var imageClip = canvatte.getNewClip();
-    imageClip.name = "sakura";
-    imageClip.addEventListener(canvatte.IMAGE_LOADED, onImageLoaded);
-    imageClip.loadImage("img/sakura2.png");
     
 function sakuraFubuki(){
     for(var index = 0; index < hanaList.length; index++){
         var hana = hanaList[index];
-       // hana.move.call(hana);
+        hana.move.call(hana);
     }
+    window.requestAnimationFrame(sakuraFubuki);
 }
+
+isDebug = true;
+
+var mainCanvas              = document.createElement("canvas");
+    mainCanvas.width        = 800;
+    mainCanvas.height       = 300;
+    mainCanvas.style.border = "solid 0.5px gray";
+    mainCanvas.id           = "MAIN_CANVAS";
+    document.body.appendChild(mainCanvas);
+
+var canvatte = new Canvate(mainCanvas);
+var hanaList = [];
+var added    = {};
+
+var imageClip      = canvatte.getNewClip();
+    imageClip.name = "sakura";
+    imageClip.addEventListener(canvatte.IMAGE_LOADED, onImageLoaded);
+    imageClip.loadImage("img/sakura2.png");
