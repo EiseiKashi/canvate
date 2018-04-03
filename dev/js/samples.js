2b8e33cbb334b4725f304ba30a1331e1246907fa
EiseiKashi <eisei.kashi@sakuracode.com>
2018-04-03 11:44:55 -0300

    Added new images for samples

---
+++ dev/js/samples.js
@@ -0,0 +1,8 @@
+var canvas1 = document.getElementById("sample1");
+var canvate1       = new Canvate(canvas1);
+
+var clip1_1 = canvate1.addNewClip();
+var clip1_2 = canvate1.addNewClip();
+var clip1_3 = canvate1.addNewClip();
+
+    clip1_1.loadImage();
